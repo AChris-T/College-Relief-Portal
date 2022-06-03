a5c26a659f0d8c1a5d123b6ccf86bd13578f44ba
Akinfenwa Taiwo Christopher <101477417+feladev123@users.noreply.github.com>
2022-06-03 12:50:18 +0100

    Add files via upload

---
+++ college.js
@@ -0,0 +1,6 @@
+let surname = "Akinfenwa";
+let firstName = "Taiwo";
+let otherName = "Christopher";
+let fullName = surname + ' ' + firstName + ' ' + otherName +",";
+
+document.getElementById("details").innerHTML = "Hello" + ' ' + fullName;
